7cfb7fa10868304fa671673c92adad403d44299e
Philip Schlump <pschlump@gmail.com>
2020-12-06 14:46:17 -0700

    Update.

--- plan.md
+++ plan.md
@@ -4,9 +4,11 @@
 
 Syllabus of Class
 
-1. Overview of Class
+1. Overview of Class - the slowest sort of all time
 2. How computer apps work (rework of lecture 10, F20-1015)
-3. What are tables (Chapter 1)
+3. Choices SQL or NoSQL
+4. Most common Structured Data - 74% of all structured data 
+3. What are tables - Structured Data (Chapter 1)
 4. ERD and Modeling (Chapter 1 & 3)
 5. More on ERD (Relations between Tables)
 6. Building tables from ERD (Chapter 2)
@@ -29,3 +31,34 @@ Create a multi-user one-db per user database at:
 
 
 https://dbdiagram.io/pricing
+
+
+Also teach NoSQL (Redis)
+
+
+System
+
+	[ Login ]
+
+	[ PostbgreSQL ] [ Redis ]
+
+	{{PG}}
+		- Each strudent has own "schema"
+		
+
+	DML / DDL
+
+	[Create Tables] [ Drop Tables ]
+
+	[ tables ] [ indexes ] [ upload ] [ import ] [ export ] [ others ]
+
+
+
+pgAdmin
+
+
+
+use a map of "username" to "connection" on d.b. if not logged in then "null".  If idle for more than X min, then close.
+
+
+ 
--- www/ct.m4.sql
+++ www/ct.m4.sql
@@ -10,13 +10,18 @@ create table ct_login (
 	, pg_acct					char varying (20) not null
 );
 
+create unique index ct_loing_u1 on ct_login ( username );
+create unique index ct_loing_u2 on ct_login ( password );
+create unique index ct_loing_u3 on ct_login ( pg_acct );
+
 -- -------------------------------------------------------- -- --------------------------------------------------------
+drop view if exists ct_video_per_user ;
 drop table if exists ct_video_seen ;
 create table ct_video_seen (
 	  user_id					char varying (40)  not null
 	, video_id					char varying (40) not null
 	, when_seen					timestamp not null
-	, whatch_count				int default 0 not null
+	, watch_count				int default 0 not null
 );
 
 -- -------------------------------------------------------- -- --------------------------------------------------------
@@ -27,9 +32,12 @@ create table ct_video (
 	, video_title				text  not null
 	, url						text not null
 	, img_url					text not null
-	, title						text not null
 	, lesson					jsonb not null	-- all the leson data from ./lesson/{video_id}.json, ./raw/{fn}
 );
+
+-- See:https://scalegrid.io/blog/using-jsonb-in-postgresql-how-to-effectively-store-index-json-data-in-postgresql/ 
+
+create index ct_video_p1 on ct_video using gin ( lesson );
 
 -- -------------------------------------------------------- -- --------------------------------------------------------
 CREATE SEQUENCE ct_run_seq
@@ -47,4 +55,137 @@ create table ct_video_questions (
 	, qdata					jsonb not null
 	, pass					char (1) default 'n' not null check ( "pass" in ( 'y','n' ) )
 );
+
+
+-- -------------------------------------------------------- -- --------------------------------------------------------
+drop table if exists ct_output ;
+create table ct_output (
+	  id			bigint DEFAULT nextval('ct_run_seq'::regclass) NOT NULL 	-- Time orderd sequece output.
+	, line			text
+	, created		timestamp default now() not null
+);
+
+create index ct_output_p1 on ct_output ( created );
+
+
+
+-- -------------------------------------------------------- -- --------------------------------------------------------
+-- See:https://www.postgresqltutorial.com/postgresql-joins/ 
+-- -------------------------------------------------------- -- --------------------------------------------------------
+create or replace view ct_video_per_user as
+	select 
+			  t1.video_id		
+			, t1.video_raw_file	
+			, t1.video_title
+			, t1.url	
+			, t1.img_url					
+			, t1.lesson					
+			, t2.when_seen
+			, t2.watch_count
+			, case			
+				when t2.watch_count = 0 then 'n'
+				when t2.watch_count is null then 'n'
+				else 'y'
+			  end as "has_been_seen"
+		from ct_video as t1
+			left outer join ct_video_seen as t2 on ( t1.video_id = t2.video_id )
+;
+
+
+
+-- -------------------------------------------------------- -- --------------------------------------------------------
+-- Data
+-- -------------------------------------------------------- -- --------------------------------------------------------
+
+insert into ct_login ( username, password, pg_acct ) values	
+	  ( 'abc', 'abc123', 'i4280v001' )
+	, ( 'abd', 'abd123', 'i4280v002' )
+;
+
+insert into ct_video ( 
+	  video_raw_file			-- text  not null
+	, video_title				-- text  not null
+	, url						-- text not null
+	, img_url					-- text not null
+	, lesson					-- jsonb not null	-- all the leson data from ./lesson/{video_id}.json, ./raw/{fn}
+) values 
+	  ( 'select_1.md', 'Intro to Select', 'select01.mp4', 'select01.jpg', '{"sample":1}' )
+	, ( 'select_2.md', 'Select From', 'select02.mp4', 'select02.jpg', '{"sample":2}' )
+;
+
+
+
+
+
+
+
+
+
+
+
+-- -------------------------------------------------------- -- --------------------------------------------------------
+-- Validation / Testing
+-- -------------------------------------------------------- -- --------------------------------------------------------
+
+CREATE or REPLACE FUNCTION ct_table_test ( p_p1 varchar, p_p2 varchar ) returns varchar AS $$
+DECLARE
+    l_junk char varying(40);
+    l_cnt int;
+    l_n_err int;
+BEGIN
+    l_n_err = 0;
+	delete from ct_output;
+
+	-- ------------------------------------------------------ -- ------------------------------------------------------
+	-- test that view is created correctly.
+	-- ------------------------------------------------------ -- ------------------------------------------------------
+	select count(1) 
+		into l_cnt
+		from ct_video_per_user
+		;
+	IF NOT FOUND THEN
+		l_n_err = l_n_err + 1;
+		insert into ct_output ( line ) values ( 'not found - no rows in view: ct_video_per_user - should be 2' );
+	ELSE
+		IF l_cnt <> 2 THEN
+			l_n_err = l_n_err + 1;
+			insert into ct_output ( line ) values ( 'Expected 2 rows got: '||l_cnt::text );
+		ELSE
+			insert into ct_output ( line ) values ( 'PASS - test 001' );
+		END IF;
+	END IF;
+
+	select count(1) 
+		into l_cnt
+		from ct_video_per_user
+		where has_been_seen = 'y'
+		;
+	IF NOT FOUND THEN
+		insert into ct_output ( line ) values ( 'PASS - test 002' );
+	ELSE
+		IF l_cnt <> 0 THEN
+			l_n_err = l_n_err + 1;
+			insert into ct_output ( line ) values ( 'Expected 0 rows got: '||l_cnt::text );
+		ELSE
+			insert into ct_output ( line ) values ( 'PASS - test 002' );
+		END IF;
+	END IF;
+
+
+
+
+
+
+	IF l_n_err = 0 THEN
+		insert into ct_output ( line ) values ( 'PASS - no errors' );
+		RETURN 'PASS - no errors';
+	ELSE	
+		RETURN 'FAIL - errors = '|| l_n_err::text;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+select ct_table_test ( '', '' );
+
+select line from ct_output order by id;
 
