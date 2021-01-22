d629c5f8ebc13b6ddb57c00706ad7c0c5c48262a
Philip Schlump <pschlump@gmail.com>
2021-01-22 06:48:53 -0700

    Example expain queries

---
+++ learn-db/hw/hw34_3.sql
@@ -0,0 +1,20 @@
+with per_user_ct_homework_grade as (
+		select * 
+		from ct_homework_grade as t4
+		where t4.user_id ='7a955820-050a-405c-7e30-310da8152b6d' 
+	)
+select
+          t1.homework_id
+        , t1.homework_no
+        , case
+            when t3.pts = 0 then 'n'
+            when t3.pts is null then 'n'
+            else 'y'
+          end as "has_been_seen"
+        , t1.homework_no::int as i_homework_no
+        , coalesce(t3.tries,NULL,0,t3.tries) as tries
+    from ct_homework as t1
+        left outer join ct_homework_seen as t2 on ( t1.homework_id = t2.homework_id )
+        left outer join per_user_ct_homework_grade as t3 on ( t1.homework_id = t3.homework_id )
+     order by 4 asc
+;
