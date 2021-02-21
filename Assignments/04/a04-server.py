3974df724b74608029915447f021dcc2892bb64d
Philip Schlump <pschlump@gmail.com>
2021-02-21 07:26:18 -0700

    Functions for Assignments

--- Assignments/04/a04-server.py
+++ Assignments/04/a04-server.py
@@ -322,17 +322,18 @@ def search_keyword():
     return run_select ( "SELECT * FROM i_issue_st_sv where words @@ to_tsquery('{}'::regconfig,%(kw)s)".format(lang), { "kw":kw[0] } )
 
 
-# @get('/api/v1/search-date-range')
-# @app.route('/api/v1/search-date-range', method=['OPTIONS', 'GET'])
-# xyzzy - TODO
-
-
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # @get('/api/v1/get-config')
 @app.route('/api/v1/get-config', method=['OPTIONS', 'GET'])
 def get_config():
     response.content_type = "application/json"
     return run_select ( "SELECT * FROM i_config", {})
 
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/issue-list
 # @get('/api/v1/issue-list')
 @app.route('/api/v1/issue-list', method=['OPTIONS', 'GET'])
@@ -340,183 +341,73 @@ def issue_list():
     return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
 
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/create-issue
 # @get('/api/v1/create-issue')
 @app.route('/api/v1/create-issue', method=['OPTIONS', 'GET'])
 def create_issue():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["title","body"]):
-        return
-    # xyzzy - PUT body params, POST
-    title = dict["title"][0]
-    body = dict["body"][0]
-    if "severity_id" in dict :
-        severity_id = dict["severity_id"][0]
-    else:
-        severity_id = "1"
-    if "state_id" in dict :
-        state_id = dict["state_id"][0]
-    else:
-        state_id = "1"
-    if "issue_id" in dict:
-        issue_id = dict["issue_id"][0]
-    else:
-        issue_id = gen_uuid()
-    # print ( "title={} body={}".format( title, body ) )
-    s = run_insert ( "insert into i_issue ( id, title, body, severity_id, state_id ) values ( %(issue_id)s, %(title)s, %(body)s, %(severity_id)s, %(state_id)s )",
-        { "title":title, "body":body, "issue_id":issue_id, "id":issue_id, "severity_id":severity_id, "state_id":state_id } )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/delete-issue
 # @get('/api/v1/delete-issue')
 @app.route('/api/v1/delete-issue', method=['OPTIONS', 'GET'])
 def delete_issue():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["issue_id"]):
-        return
-    issue_id = dict["issue_id"][0]
-    if False:
-        # Delete any notes first - to get past the FK
-        s = run_delete ( "delete from i_note where issue_id = %(issue_id)s ", { "issue_id":issue_id } )
-        # print ( "delete i_note = {}".format(s) )
-        s = run_delete ( "delete from i_issue where id = %(issue_id)s ", { "issue_id":issue_id } )
-    else:
-        s = run_delete ( "update i_issue set state_id = ( select id from i_state where state = 'Deleted' )  where id = %(issue_id)s ", { "issue_id":issue_id } )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/update-issue
 # @get('/api/v1/update-issue')
 @app.route('/api/v1/update-issue', method=['OPTIONS', 'GET'])
 def update_issue():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["title","body","issue_id"]):
-        return
-    # xyzzy - PUT body params, POST
-    title = dict["title"][0]
-    body = dict["body"][0]
-    issue_id = dict["issue_id"][0]
-    if "severity_id" in dict :
-        severity_id = dict["severity_id"][0]
-        stmt_sev = ", severity_id = %(severity_id)s "
-    else:
-        severity_id = "1"
-        stmt_sev = ""
-    if "state_id" in dict :
-        state_id = dict["state_id"][0]
-        stmt_state = ", state_id = %(state_id)s "
-    else:
-        state_id = "1"
-        stmt_state = ""
-    stmt = "update i_issue set title = %(title)s, body = %(body)s "+stmt_sev+stmt_state+" where id = %(issue_id)s"
-    data = { "title":title, "body":body, "issue_id":issue_id, "severity_id":severity_id, "state_id":state_id } 
-    # print ( "stmt= ->{}<- data={}".format ( stmt, data ) )
-    s = run_update ( stmt, data )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
 
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/v1/get-issue-details - get an issue with all of its notes
 # @get('/api/v1/get-issue-detail')
 @app.route('/api/v1/get-issue-detail', method=['OPTIONS', 'GET'])
 def get_issue_detail():
-    response.content_type = "application/json"
-
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["issue_id"]):
-        return
-    issue_id = dict["issue_id"][0]
-
-    issue_data = run_select_raw ( "SELECT * FROM i_issue where id = %(issue_id)s", { "issue_id":issue_id } )
-
-    issue_result = issue_data["result"]
-
-    if issue_data["n_rows"] == 1 :
-        note_data = run_select_raw ( "SELECT * FROM i_note where issue_id = %(issue_id)s order by seq", { "issue_id":issue_id } )
-        issue_result[0]["note"] = note_data["result"]
-        issue_result[0]["n_rows_note"] = note_data["n_rows"]
-    else:
-        issue_result[0]["note"] = []
-        issue_result[0]["n_rows_note"] = 0
-
-    ss = json.dumps(
-      issue_result,
-      sort_keys=True,
-      indent=1,
-      default=default
-    )
-
-    return "{"+"\"status\":\"success\",\"n_rows\":1,\"data\":{}".format(ss)+"}"
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
     
-
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/add-note-to-issue
 # @get('/api/v1/add-note-to-issue')
 @app.route('/api/v1/add-note-to-issue', method=['OPTIONS', 'GET'])
 def create_note():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["title","body","issue_id"]):
-        return
-    # xyzzy - PUT body params, POST
-    # print ( "dict={}".format(dict))
-    title = dict["title"][0]
-    body = dict["body"][0]
-    issue_id = dict["issue_id"][0]
-    note_id = gen_uuid()
-    # note_id --- return this ID
-    # print ( "title={} body={}".format( title, body ) )
-    s =  run_insert ( "insert into i_note ( id, issue_id, title, body ) values ( %(note_id)s, %(issue_id)s, %(title)s, %(body)s )",
-        { "title":title, "body":body, "note_id":note_id, "issue_id":issue_id, "id":note_id } )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/upd-note
 # @get('/api/v1/update-note')
 @app.route('/api/v1/delete-note', method=['OPTIONS', 'GET'])
 def update_note():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["title","body","note_id"]) :
-        return
-    # xyzzy - PUT body params, POST
-    title = dict["title"][0]
-    body = dict["body"][0]
-    note_id = dict["note_id"][0]
-    s = run_update ( "update i_note set title = %(title)s, body = %(body)s where id = %(note_id)s", { "title":title, "body":body, "note_id":note_id, "id":note_id } )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
 
-
-# xyzzyA04
+#--------------------------------------------------------------------------------------------------------
+# Assignment 04
+#--------------------------------------------------------------------------------------------------------
 # /api/upd-severity
 # @get('/api/v1/update-severity')
 @app.route('/api/v1/update-severity', method=['OPTIONS', 'GET'])
 def upd_severity():
-    global db_conn
-    response.content_type = "application/json"
-    dict = parse_qs(request.query_string)
-    if not required_param(dict,["severity_id","issue_id"]) :
-        return
-    severity_id = dict["severity_id"][0]
-    issue_id = dict["issue_id"][0]
-    s = run_update ( "update i_issue set severity_id = %(severity_id)s where id = %(issue_id)s", { "severity_id":severity_id, "issue_id":issue_id } )
-    db_conn.commit()
-    return s
+    return "{"+"\"status\":\"TODO\",\"n_rows\":0,\"data\":[]"+"}"
+
+
+
+
 
 # /api/upd-state
 # @get('/api/v1/update-state')
@@ -533,15 +424,12 @@ def upd_state():
     db_conn.commit()
     return s
 
-
-# xyzzyA04
 # @get('/api/v1/get-state')
 @app.route('/api/v1/get-state', method=['OPTIONS', 'GET'])
 def get_state():
     response.content_type = "application/json"
     return run_select ( "SELECT * FROM i_state", {})
 
-# xyzzyA04
 # @get('/api/v1/get-severity')
 @app.route('/api/v1/get-severity', method=['OPTIONS', 'GET'])
 def get_severity():
@@ -550,7 +438,6 @@ def get_severity():
 
 
 
-# xyzzyA04
 @app.route('/api/v1/note', method=['OPTIONS', 'DELETE'])
 def delete_note():
     response.content_type = "application/json"
@@ -560,7 +447,6 @@ def delete_note():
     note_id = lower(dict["note_id"][0])
     return run_delete ( "DELETE FROM i_note WHERE id = %(id)s", { "id": note_id })
 
-# xyzzyA04
 @app.route('/api/v1/note', method=['OPTIONS', 'PUT'])
 def update_note():
     response.content_type = "application/json"
@@ -572,7 +458,6 @@ def update_note():
     body = lower(dict["body"][0])
     return run_update ( "UPDATE i_note SET title = %(title)s, body = %(body)s WHERE id = %(id)s", { "id": note_id })
 
-# xyzzyA04
 @app.route('/api/v1/get-note', method=['OPTIONS', 'GET'])
 def update_note():
     response.content_type = "application/json"
@@ -589,7 +474,6 @@ def update_note():
 # RESTful Interface to issue, note
 #################################################################################################################################
 
-# xyzzyA04
 # Issue ---------------------------------------------------------------------------
 # on get - if __method__=X then do X instead
 # @get('/api/v1/issue')
@@ -635,7 +519,6 @@ def issue_post():
 
 # Note ---------------------------------------------------------------------------
 
-# xyzzyA04
 # @get('/api/v1/note')
 @app.route('/api/v1/note', method=['OPTIONS', 'GET'])
 def issue_get():
@@ -650,13 +533,11 @@ def issue_get():
             return update_note()
     return note_list()
 
-# xyzzyA04
 # @put('/api/v1/note')
 @app.route('/api/v1/note', method=['OPTIONS', 'PUT'])
 def note_put():
     return update_note()
 
-# xyzzyA04
 # @post('/api/v1/note')
 @app.route('/api/v1/note', method=['OPTIONS', 'POST'])
 def note_post():
@@ -666,7 +547,6 @@ def note_post():
 # State ---------------------------------------------------------------------------
 # @get('/api/v1/get-state')
 
-# xyzzyA04
 # @get('/api/v1/state')
 @app.route('/api/v1/state', method=['OPTIONS', 'GET'])
 def state_get():
@@ -677,7 +557,6 @@ def state_get():
 # Severity ---------------------------------------------------------------------------
 # @get('/api/v1/get-severity')
 
-# xyzzyA04
 # @get('/api/v1/severity')
 @app.route('/api/v1/severity', method=['OPTIONS', 'GET'])
 def severity_get():
